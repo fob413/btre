39d839b2538bd330673af6ce4d2336911e2978b2
fob1493 <fob1493@gmail.com>
2018-12-19 22:09:54 +0100

    feat(listing): Pagination:
    - Paginate the listing data

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from .models import Listing
 
 # Create your views here.
 def index(request):
-  return render(request, 'listings/listings.html')
+  listings = Listing.objects.order_by("-list_date").filter(is_published=True)
+  paginator = Paginator(listings, 3)
 
-def listing(request):
+  page = request.GET.get('page')
+  paged_listing = paginator.get_page(page)
+
+  context = {
+    'Listings': paged_listing
+  }
+  return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
   return render(request, 'listings/listing.html')
 
 def search(request):
